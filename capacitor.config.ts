8a9f2e18fa12ade3eacd007bc501cbaa0692e7ea
Pedro Zurita <pe.zurita@duocuc.cl>
2024-11-03 16:03:52 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demop2',
+  webDir: 'www'
+};
+
+export default config;
